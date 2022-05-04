03cf106684f0d39b7cc8c70cddcea70d23a52c2b
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-05-04 09:16:25 -0500

    Challenge Backend initialization

---
+++ src/main/java/com/webdev/challenge/challengebackend/ChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.challenge.challengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdev/challenge/challengebackend/ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.challenge.challengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
